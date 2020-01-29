2c4377121a2998213d2211ab5ce0e900054838e3
karenjli <karenjy.li@gmail.com>
2020-01-28 18:00:28 -0600

    MVP 1st attempt

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,9 @@
+
+from doubly_linked_list import ListNode, DoublyLinkedList
+import sys
+sys.path.append('./doubly_linked_list')
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +12,12 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
-    def __init__(self, limit=10):
-        pass
+
+    def __init__(self,  limit=10):
+        self.limit = limit
+        self.size = 0
+        self.list = DoublyLinkedList()
+        self.cache = dict()
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +26,19 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
     def get(self, key):
-        pass
+        # Is key in cache?
+        if key in self.cache.keys():
+            # node equals the value of the key
+            node = self.cache[key]
+            # Add the key-value pair to the head of the list
+            self.list.move_to_front(node)
+            # Return the first value of the list
+            return node.value[1]
+        else:
+            # Return none if key doesn't exist
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +50,21 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
-        pass
+        # Check if key is already there
+        if key in self.cache.keys():
+            # If yes, identify the node
+            node = self.cache[key]
+            node.value = (key, value)
+            self.list.move_to_front(node)
+            return self.list.head
+        else:
+            if self.size == self.limit:
+                del self.cache[self.list.remove_from_tail()[0]]
+                self.size -= 1
+
+            new_cache = (key, value)
+            self.list.add_to_head(new_cache)
+            self.cache[key] = self.list.head
+            self.size += 1
